3fd7c9ebd14e2d33d4824358a38094fc14c3ea58
fabertleury <fabert_@hotmail.com>
2025-05-02 13:47:51 -0300

    ajuste site

--- src/app/api/recovery/cart/route.ts
+++ src/app/api/recovery/cart/route.ts
@@ -10,7 +10,7 @@ const pool = new Pool({
 
 // Configuração do Supabase
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
+const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY || '';
 const supabase = createClient(supabaseUrl, supabaseServiceKey);
 
 // Configuração do transportador de email
